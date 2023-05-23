bd07eca862f2dc18f8057a7205bd6f87ec248aa5
MostafaElSayed-FCDS <mostafaelsayed2172000@gmail.com>
2023-05-23 15:00:25 -0700

    C

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h> /**for( _exit,access,chdir,close,execve,fork,_exit,
+					*getpid,isatty,kill,open,read,write )**/
+#include <stdio.h> /for fflush/
+#include <stdlib.h> /*for (exit,free,malloc)*/
+#include <dirent.h> /*for (closedir,opendir,readdir)*/
+#include <string.h> /*for (strtok)*/
+#include <signal.h> /*for (signal)*/
+#include <sys/stat.h> /*for (stat,lstat,fstat) */
+#include <errno.h> /*for (perror)*/
+#include <sys/wait.h>/*for (wait,wait,pid,wait3,wait4)*/
+#include <stdio.h>/for using null/
+
+void print_environment(void);
+void search_command(char **args);
+
+#endif
