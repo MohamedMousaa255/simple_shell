f4663c7a7f191d4a38179b25f582ebcf39c48936
mohamed moussa <mohamedmousa2552001@gmail.com>
2023-05-23 23:41:23 +0200

    update

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+/**
+ * main - Main function
+ *@argc: argument counter
+ *@argv: argumentlist
+ * Return: Returns 0
+ */
+int main(int argc, char *argv[])
+{
+    char **args = argv + 1;
+    pid_t pid;
+    int count;
+
+    while (1)
+    {
+        if (argc < 2)
+        {
+            write(STDOUT_FILENO, "No command entered.\n", 20);
+            break;
+        }
+
+        if (strcmp(args[0], "exit") == 0)
+            break;
+        else if (strcmp(args[0], "env") == 0)
+        {
+            print_environment();
+            break;
+        }
+
+        pid = fork();
+        if (pid == 0)
+        {
+            count = argc - 1;
+            args[count] = NULL;
+
+            search_command(args);
+        }
+        else
+            wait(NULL);
+        break;
+    }
+
+    return (0);
+}
+
