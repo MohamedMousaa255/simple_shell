a3907794ef099b222689f6960c40d7d795a5d182
MostafaElSayed-FCDS <mostafaelsayed2172000@gmail.com>
2023-05-23 15:35:26 -0700

    C

--- searchcomments.c
+++ searchcomments.c
@@ -7,39 +7,40 @@
  */
 void search_command(char **args)
 {
-	char *command, *path, *path_copy, *directory, *full_command;
+    char *command, *path, *path_copy, *directory, *full_command;
 
-	command = strdup(args[0]);
-	if (command == NULL)
-		exit(1);
+    command = strdup(args[0]);
+    if (command == NULL)
+        exit(1);
 
-	if (access(command, X_OK) == -1)
-	{
-		path = getenv("PATH");
-		path_copy = strdup(path);
-		directory = strtok(path_copy, ":");
+    if (access(command, X_OK) == -1)
+    {
+        path = getenv("PATH");
+        path_copy = strdup(path);
+        directory = strtok(path_copy, ":");
 
-		while (directory != NULL)
-		{
-			full_command = malloc(strlen(directory) + strlen(command) + 2);
-			if (full_command == NULL)
-				exit(1);
-			sprintf(full_command, "%s/%s", directory, command);
-			if (access(full_command, X_OK) == 0)
-			{
-				args[0] = full_command;
-				break;
-			}
-			free(full_command);
-			directory = strtok(NULL, ":");
-		}
-		free(path_copy);
-		free(command);
-	}
+        while (directory != NULL)
+        {
+            full_command = malloc(strlen(directory) + strlen(command) + 2);
+            if (full_command == NULL)
+                exit(1);
+            sprintf(full_command, "%s/%s", directory, command);
+            if (access(full_command, X_OK) == 0)
+            {
+                args[0] = full_command;
+                break;
+            }
+            free(full_command);
+            directory = strtok(NULL, ":");
+        }
+        free(path_copy);
+        free(command);
+    }
 
-	if (execvp(args[0], args) == -1)
-	{
-		printf("%s: command not found\n", args[0]);
-		exit(1);
-	}
+    if (execvp(args[0], args) == -1)
+    {
+        char error_message[50];
+        write(STDERR_FILENO, error_message, strlen(error_message));
+        exit(1);
+    }
 }
--- shell.h
+++ shell.h
@@ -3,7 +3,7 @@
 
 #include <unistd.h> /**for( _exit,access,chdir,close,execve,fork,_exit,
 					*getpid,isatty,kill,open,read,write )**/
-#include <stdio.h> /for fflush/
+#include <stdio.h> /*for fflush*/
 #include <stdlib.h> /*for (exit,free,malloc)*/
 #include <dirent.h> /*for (closedir,opendir,readdir)*/
 #include <string.h> /*for (strtok)*/
@@ -11,7 +11,9 @@
 #include <sys/stat.h> /*for (stat,lstat,fstat) */
 #include <errno.h> /*for (perror)*/
 #include <sys/wait.h>/*for (wait,wait,pid,wait3,wait4)*/
-#include <stdio.h>/for using null/
+#include <stdio.h> /*for using null*/
+#include <unistd.h> /*access to POSIX API functions*/
+#include <fcntl.h> /*file control options*/
 
 void print_environment(void);
 void search_command(char **args);
